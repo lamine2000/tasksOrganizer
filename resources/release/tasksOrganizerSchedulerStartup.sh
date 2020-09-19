cb084e40e9610302624e73249c02c4ecb28a1f7c
lamine2000 <lemzogueye.lg72@gmail.com>
2020-09-19 01:20:28 +0000

    corraction d'erreurs du script .sh

--- resources/release/tasksOrganizerSchedulerStartup.sh
+++ resources/release/tasksOrganizerSchedulerStartup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-desktopFile=tasksOrganizerScheduler.desktop
 
-#jarPath doit contenir l'emplacement de tasksOrganizer.jar
-jarPath=$HOME/IdeaProjects/tasksOrganizer/out/artifacts/tasksOrganizer.jar
+desktopFile="$HOME"/.config/autostart/tasksOrganizerScheduler.desktop
 
-touch "$HOME"/.config/autostart/$desktopFile
+jarPath="$HOME"/IdeaProjects/tasksOrganizer/out/artifacts/tasksOrganizer.jar
 
-desktop-file-edit --set-key=Type --set-value=Application --set-key=Exec --set-value="java -jar $jarPath hide" --set-key=Hidden --set-value=false --set-key=NoDisplay --set-value=false --set-key=Name --set-value="tasksOrganizer.jar scheduler startUp" --set-key=X-GNOME-Autostart-enabled --set-value=true $desktopFile
+touch "$desktopFile"
+
+desktop-file-edit --set-key=Type --set-value=Application --set-key=Exec --set-value="java -jar $jarPath hide" --set-key=Hidden --set-value=false --set-key=NoDisplay --set-value=false --set-key=Name --set-value="tasksOrganizer.jar scheduler startUp" --set-key=X-GNOME-Autostart-enabled --set-value=true "$desktopFile"
