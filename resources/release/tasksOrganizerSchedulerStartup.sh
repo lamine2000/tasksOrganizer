bf810e2a3b858c2e899d50285f7cb91b4b404903
lamine <lemzogueye.lg72@gmail.com>
2021-09-22 14:10:33 +0000

    Le fichier jar doit être placé dans /opt/tasksOrganizer

--- resources/release/tasksOrganizerSchedulerStartup.sh
+++ resources/release/tasksOrganizerSchedulerStartup.sh
@@ -2,8 +2,8 @@
 
 desktopFile="$HOME"/.config/autostart/tasksOrganizerScheduler.desktop
 
-jarPath="$HOME"/IdeaProjects/tasksOrganizer/resources/release/tasksOrganizer.jar
+jarPath=/opt/tasksOrganizer/tasksOrganizer.jar
 
 touch "$desktopFile"
 
-desktop-file-edit --set-key=Type --set-value=Application --set-key=Exec --set-value="java -jar $jarPath hide" --set-key=Hidden --set-value=false --set-key=NoDisplay --set-value=false --set-key=Name --set-value="tasksOrganizer.jar scheduler startUp" --set-key=X-GNOME-Autostart-enabled --set-value=true "$desktopFile"
+desktop-file-edit --set-key=Type --set-value=Application --set-key=Exec --set-value="java16 -jar $jarPath hide" --set-key=Hidden --set-value=false --set-key=NoDisplay --set-value=false --set-key=Name --set-value="tasksOrganizer.jar scheduler startUp" --set-key=X-GNOME-Autostart-enabled --set-value=true "$desktopFile"
